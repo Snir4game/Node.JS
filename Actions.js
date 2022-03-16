d91cf632b1385622cfdd9c2753b84dc74fb39ffd
Sniro <97622458+Snir4game@users.noreply.github.com>
2022-03-16 10:25:41 +0200

    Update Actions.js

--- Actions.js
+++ Actions.js
@@ -1,5 +1,23 @@
+const { request } = require('express');
 const express =require('express');
+const res = require('express/lib/response');
 const router =express.Router();
+
+let users=[];
+
+
+router.post('/register',(request,response)=>{
+    //const user_email=request.body.email;
+    //const user_password=request.body.password;
+ 
+    //אופציה ב
+    const{email,password}=request.body;
+
+    users.push({email:email,password:password});
+    return response.status(200).json({
+    message:users
+    });
+})
 
 router.get('/Welcome',(request,response)=>{
 
