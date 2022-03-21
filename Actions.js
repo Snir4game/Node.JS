fca30fe5c82a948dc4a7c8f592caf2efe0416f08
Sniro <97622458+Snir4game@users.noreply.github.com>
2022-03-21 12:30:35 +0200

    Update Actions.js

--- Actions.js
+++ Actions.js
@@ -1,9 +1,26 @@
 const { request } = require('express');
 const express =require('express');
+const { status } = require('express/lib/response');
 const res = require('express/lib/response');
 const router =express.Router();
 
-let users=[];
+let users=[
+
+    {
+       email:'Sniramsalem1995@gmail.com',
+        password:'Qwerty1234',
+
+    },
+    {
+        email: 'Amir1221@gmail.com',
+        password:'Asd123'
+    },
+    {
+        email:'aviv1999@walla.com',
+        password:'123456'
+    }
+
+];
 
 
 router.post('/register',(request,response)=>{
@@ -11,12 +28,37 @@ router.post('/register',(request,response)=>{
     //const user_password=request.body.password;
  
     //אופציה ב
-    const{email,password}=request.body;
+    const {email, password} = request.body;
+    const data = users.find(userEmail => users.email == email)
+    if(data){
+        return response.status(200).json({
+            message: 'This email exist try another one'
+        })
+    } else {
+        users.push({email:email, password: password})
+        return response.status(200).json({
+            message: `Welcome ${email}`
+        })
+    }
+    // const temp=users.find(x=>x.users.email=='Sniramsalem1995@gmail.com');
+    // if(email==users.email &&password==users.password)
+    // {
+    //     return response.status(200).json({
+    //         message:'this email is already Exists'
+    //     });
+    // }
+    // else
+    // {
+    //     return response.status(200).json({
+    //         message:`Welcome ${x.temp}`
+    //     })
+    // }
+    // const{email,password}=request.body;
+    // users.push({email:email,password:password});
     
-    users.push({email:email,password:password});
-    return response.status(200).json({
-    message:users
-    });
+    // return response.status(200).json({
+    // message:users
+    // });
 })
 
 router.get('/Welcome',(request,response)=>{
